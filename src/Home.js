992cfc251f58b6334d5f228fef61ce7821cab28a
akshmeh <rishimehta257428@gmail.com>
2021-07-15 15:09:24 +0530

    update

--- src/Home.js
+++ src/Home.js
@@ -13,7 +13,7 @@ import {
 export const Home = () => {
     return (
         <>
-            <div className="avatar img-fluid">
+            <div className="avatar img-fluid mx-auto">
                 <img className="avatar__image" src={person} alt="person"/>
             </div>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#353434" fill-opacity="1" d="M0,128L48,144C96,160,192,192,288,181.3C384,171,480,117,576,106.7C672,96,768,128,864,170.7C960,213,1056,267,1152,256C1248,245,1344,171,1392,133.3L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>            <div className="person-details text-white text-left">
