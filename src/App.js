cbb67b943e03b7726fa955dcc3bc990b5537148c
akshmeh <rishimehta257428@gmail.com>
2021-08-28 16:12:04 +0530

    updated website

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {Navbar} from "./Navbar";
 import {Home} from "./Home";
 // import {About} from "./About";
 import {Work} from "./Work";
-// import {Contact} from "./Contact";
+// import {Back} from "./Back";
 
 
 function App() {
@@ -33,25 +33,27 @@ function App() {
           {/* <Route exact path="/aksh.at/Contact">
             <Contact/>
           </Route> */}
-          <Route path='/aksh.at/facebook' component={() => { 
-              // window.location.href = 'https://www.facebook.com/akshat.mehta.522'; 
+          <Route exact path='/aksh.at/facebook' component={() => { 
+            //  window.location.href = 'https://www.facebook.com/akshat.mehta.522'; 
+            // window.location.href = 'https://akshmeh.github.io/aksh.at/';
+
               window.open('https://www.facebook.com/akshat.mehta.522', "_blank")
-              return null;
+              return window.location.href = 'https://akshmeh.github.io/aksh.at/';
           }}/>
-          <Route path='/aksh.at/instagram' component={() => { 
+          <Route exact path='/aksh.at/instagram' component={() => { 
               // window.location.href = 'https://instagram.com/akshatmehta74?utm_medium=copy_link'; 
               window.open('https://instagram.com/akshatmehta74?utm_medium=copy_link', "_blank")
-              return null;
+              return window.location.href = 'https://akshmeh.github.io/aksh.at/';
           }}/>
-          <Route path='/aksh.at/snapchat' component={() => { 
+          <Route exact path='/aksh.at/snapchat' component={() => { 
               // window.location.href = 'https://www.snapchat.com/add/rishimehta74744'; 
               window.open('https://www.snapchat.com/add/rishimehta74744', "_blank")
-              return null;
+              return window.location.href = 'https://akshmeh.github.io/aksh.at/';
           }}/>
-          <Route path='/aksh.at/linkedin' component={() => { 
+          <Route exact path='/aksh.at/linkedin' component={() => { 
               // window.location.href = 'https://www.linkedin.com/in/akshat-mehta-981100172'; 
               window.open('https://www.linkedin.com/in/akshat-mehta-981100172', "_blank")
-              return null;
+              return window.location.href = 'https://akshmeh.github.io/aksh.at/';
           }}/>
           <Route path='/aksh.at/contact' component={() => { 
               window.location.href = 'https://akshmeh.github.io/aksh.at#contact'; 
