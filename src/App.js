a83d74bf4e4672d198b440c7748267cceb329d4b
akshmeh <rishimehta257428@gmail.com>
2021-08-28 15:14:58 +0530

    social bug

--- src/App.js
+++ src/App.js
@@ -34,19 +34,23 @@ function App() {
             <Contact/>
           </Route> */}
           <Route path='/aksh.at/facebook' component={() => { 
-              window.location.href = 'https://www.facebook.com/akshat.mehta.522'; 
+              // window.location.href = 'https://www.facebook.com/akshat.mehta.522'; 
+              window.open('https://www.facebook.com/akshat.mehta.522', "_blank")
               return null;
           }}/>
           <Route path='/aksh.at/instagram' component={() => { 
-              window.location.href = 'https://instagram.com/akshatmehta74?utm_medium=copy_link'; 
+              // window.location.href = 'https://instagram.com/akshatmehta74?utm_medium=copy_link'; 
+              window.open('https://instagram.com/akshatmehta74?utm_medium=copy_link', "_blank")
               return null;
           }}/>
           <Route path='/aksh.at/snapchat' component={() => { 
-              window.location.href = 'https://www.snapchat.com/add/rishimehta74744'; 
+              // window.location.href = 'https://www.snapchat.com/add/rishimehta74744'; 
+              window.open('https://www.snapchat.com/add/rishimehta74744', "_blank")
               return null;
           }}/>
           <Route path='/aksh.at/linkedin' component={() => { 
-              window.location.href = 'https://www.linkedin.com/in/akshat-mehta-981100172'; 
+              // window.location.href = 'https://www.linkedin.com/in/akshat-mehta-981100172'; 
+              window.open('https://www.linkedin.com/in/akshat-mehta-981100172', "_blank")
               return null;
           }}/>
           <Route path='/aksh.at/contact' component={() => { 
